2c2fa583791ebadccc77f91ed95e510a105acdb4
oldmaimai <maihuaan@126.com>
2021-05-30 22:40:36 +0800

    2021/5/28 流量监控

--- src/main/java/com/huaan/javabasic/system/NetWorkUtil.java
+++ src/main/java/com/huaan/javabasic/system/NetWorkUtil.java
@@ -62,50 +62,51 @@ public class NetWorkUtil {
         result.put("rxPercent", rxPercent);// 下行速率
         result.put("txPercent", txPercent);// 上行速率
         return result;
-
-
-        private static long[] readInLine (BufferedReader input, String osType){
-            long arr[] = new long[2];
-            StringTokenizer tokenStat = null;
-            try {
-                if (osType.equals("linux")) { // 获取linux环境下的网口上下行速率
-                    long rx = 0, tx = 0;
-                    String line = null;
-                    //RX packets:4171603 errors:0 dropped:0 overruns:0 frame:0
-                    //TX packets:4171603 errors:0 dropped:0 overruns:0 carrier:0
-                    while ((line = input.readLine()) != null) {
-                        if (line.indexOf("RX packets") >= 0) {
-                            rx += Long.parseLong(line.substring(line.indexOf("RX packets") + 11, line.indexOf(" ", line.indexOf("RX packets") + 11)));
-                        } else if (line.indexOf("TX packets") >= 0) {
-                            tx += Long.parseLong(line.substring(line.indexOf("TX packets") + 11, line.indexOf(" ", line.indexOf("TX packets") + 11)));
-                        }
-                    }
-                    arr[0] = rx;
-                    arr[1] = tx;
-                } else { // 获取windows环境下的网口上下行速率
-                    input.readLine();
-                    input.readLine();
-                    input.readLine();
-                    input.readLine();
-                    tokenStat = new StringTokenizer(input.readLine());
-                    tokenStat.nextToken();
-                    arr[0] = Long.parseLong(tokenStat.nextToken());
-                    arr[1] = Long.parseLong(tokenStat.nextToken());
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return arr;
-        }
-
-        private static String formatNumber(double f){
-            return new Formatter().format("%.2f", f).toString();
-        }
-
-        public static void main (String[]args){
-            Map<String, String> result = getNetworkDownUp();
-            System.out.println(result.get("rxPercent"));
-            System.out.println(result.get("txPercent"));
-
-        }
     }
+
+    private static long[] readInLine(BufferedReader input, String osType) {
+        long arr[] = new long[2];
+        StringTokenizer tokenStat = null;
+        try {
+            if (osType.equals("linux")) { // 获取linux环境下的网口上下行速率
+                long rx = 0, tx = 0;
+                String line = null;
+                //RX packets:4171603 errors:0 dropped:0 overruns:0 frame:0
+                //TX packets:4171603 errors:0 dropped:0 overruns:0 carrier:0
+                while ((line = input.readLine()) != null) {
+                    if (line.indexOf("RX packets") >= 0) {
+                        rx += Long.parseLong(line.substring(line.indexOf("RX packets") + 11, line.indexOf(" ", line.indexOf("RX packets") + 11)));
+                    } else if (line.indexOf("TX packets") >= 0) {
+                        tx += Long.parseLong(line.substring(line.indexOf("TX packets") + 11, line.indexOf(" ", line.indexOf("TX packets") + 11)));
+                    }
+                }
+                arr[0] = rx;
+                arr[1] = tx;
+            } else { // 获取windows环境下的网口上下行速率
+                input.readLine();
+                input.readLine();
+                input.readLine();
+                input.readLine();
+                tokenStat = new StringTokenizer(input.readLine());
+                tokenStat.nextToken();
+                arr[0] = Long.parseLong(tokenStat.nextToken());
+                arr[1] = Long.parseLong(tokenStat.nextToken());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return arr;
+    }
+
+    private static String formatNumber(double f) {
+        return new Formatter().format("%.2f", f).toString();
+    }
+
+
+    public static void main(String[] args) {
+        Map<String, String> result = getNetworkDownUp();
+        System.out.println(result.get("rxPercent"));
+        System.out.println(result.get("txPercent"));
+
+    }
+}
